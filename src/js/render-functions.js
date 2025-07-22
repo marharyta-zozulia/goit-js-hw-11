a6f561fa0209fc2888c469225e5b2fa71abb4638
marharyta zozulia <zozulia.marharyta@gmail.com>
2025-07-22 16:49:26 +0300

    editing

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -14,10 +14,30 @@ export function createGallery(images) {
         <a href="${img.largeImageURL}">
           <img src="${img.webformatURL}" alt="${img.tags}" />
         </a>
+       <<div class="info">
+      <p>Likes: ${img.likes}</p>
+      <p>Views: ${img.views}</p>
+      <p>Comments: ${img.comments}</p>
+      <p>Downloads: ${img.downloads}</p>
+    </div>
+
       </li>
     `).join('');
   
     gallery.insertAdjacentHTML('beforeend', markup);
     lightbox.refresh();
 
-  }
+}
+  
+export function clearGallery() {
+    gallery.innerHTML = '';
+}
+  
+
+export function showLoader() {
+    loader.classList.add('visible'); 
+}
+  
+export function hideLoader() {
+  loader.classList.remove('visible'); 
+}
